a9eca28631a0b1fb2e49c37c24e9bf4ec2f2e4f5
ismrnobody <millanquintana9@gmail.com>
2024-05-12 10:51:45 -0400

    backdropp

--- components/BannerHome.vue
+++ components/BannerHome.vue
@@ -57,10 +57,10 @@
   }
   .bgc {
     background: rgba(255, 255, 255, 0.22);
-    border-radius: 16px;
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    -webkit-backdrop-filter: blur(7.1px);
     backdrop-filter: blur(7.1px);
+    border-radius: 16px;
+    /* -webkit-backdrop-filter: blur(7.1px); */
     border: 1px solid rgba(255, 255, 255, 0.3);
   }
 </style>
