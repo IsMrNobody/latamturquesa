60858b5f52294e960ccd9d2ff9be160be957e675
ismrnobody <millanquintana9@gmail.com>
2024-05-12 10:55:26 -0400

    backdropdiv

--- components/BannerHome.vue
+++ components/BannerHome.vue
@@ -9,21 +9,23 @@
         <v-container>
           <v-row>
             <v-col class="topm">
-              <v-card
-                class="bgc"
-                flat
-                max-width="600"
-              >
-                <h1 class="ma-2">Services worldwide</h1>
-                <p class="ma-2 display-1">Explore business opportunities, expert consulting, and unique travel experiences.</p>
-                <v-card-actions>
-                  <v-btn class="bton" x-large outlined>Explore more</v-btn>
-                </v-card-actions>
-              <v-img
-                class="d-flex d-sm-none ma-4"
-                src="/mapa.png">
-              </v-img>
-              </v-card>
+              <div class="bgc">
+                <v-card
+                  class="transparent"
+                  flat
+                  max-width="600"
+                >
+                  <h1 class="ma-2">Services worldwide</h1>
+                  <p class="ma-2 display-1">Explore business opportunities, expert consulting, and unique travel experiences.</p>
+                  <v-card-actions>
+                    <v-btn class="bton" x-large outlined>Explore more</v-btn>
+                  </v-card-actions>
+                <v-img
+                  class="d-flex d-sm-none ma-4"
+                  src="/mapa.png">
+                </v-img>
+                </v-card>
+              </div>
             </v-col>
             <!-- <v-col align="center">
               <v-img class="d-flex d-sm-none" height="100vh" src="/mapa.png"></v-img>
@@ -59,8 +61,9 @@
     background: rgba(255, 255, 255, 0.22);
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(7.1px);
+    -webkit-backdrop-filter: blur(7.1px);
     border-radius: 16px;
-    /* -webkit-backdrop-filter: blur(7.1px); */
     border: 1px solid rgba(255, 255, 255, 0.3);
+    width: 600px;
   }
 </style>
